5991977b4edb0e3a271e0d00af847b97fe818a6a
H <18203627037@163.com>
2018-10-29 17:40:40 +0800

    11

--- src/main/java/cn/itcast/demo.java
+++ src/main/java/cn/itcast/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("demo");
         System.out.println(" 1234r");
+        System.out.println("1234567");
     }
 }
